10d589928537eccfb91425a8cf54d708bebeef1f
perep797 <perep797@newschool.edu>
2024-09-25 20:15:31 -0400

    LET ME COMMIT I SWEAR TO GOD

--- unit-1/lesson-3/assignment3.py
+++ unit-1/lesson-3/assignment3.py
@@ -5,3 +5,4 @@ word_list.pop(1)
 print(word_list)
 
 
+#I have no idea where exactly the data set is supposed to come from, so I made my own
--- unit-1/lesson-3/assignment3part2.py
+++ unit-1/lesson-3/assignment3part2.py
@@ -1,4 +1,10 @@
-#pillow/pip are not working......... I think theres issues with the file paths on my computer
+import sys
 from PIL import Image
 
-print("10")
+if len(sys.argv) != 3:
+    exit("This command requires two arguments: the name of an image file and amount of rotation")
+
+img = Image.open( sys.argv[1] )
+rotated_img = img.rotate( int(sys.argv[2]) )
+rotated_img.save("rotated-" + sys.argv[1])
+#had no problems running this 
